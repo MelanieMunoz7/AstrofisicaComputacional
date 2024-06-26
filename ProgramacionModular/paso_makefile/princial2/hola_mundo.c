7df5a296fce32aa1afab8e0ae8fe1b2cece6ab74
MelanieMunoz7 <141173067+MelanieMunoz7@users.noreply.github.com>
2024-06-26 11:51:57 -0500

    Add files via upload

---
+++ ProgramacionModular/paso_makefile/princial2/hola_mundo.c
@@ -0,0 +1,14 @@
+#include <funcion.h> 
+
+int main() 
+{ 
+    escribeHolaMundo ();
+    return 0;
+}
+
+/*
+CFLAGS=-I../funcion2
+export CLFAGS 
+make HolaMundo, fallara porque tiene dos archivos .c
+ */
+// compilar con el Makefile
